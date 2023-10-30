d1fcc7fefbf47aaafdf3890bd00bb10c7f16ea97
vimal-1963 <vmathew7@my.centennialcollege.ca>
2023-10-30 14:31:25 -0400

    initial commit

---
+++ src/main/java/com/vimal/maven/project/VimalMavenProjectJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.vimal.maven.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimalMavenProjectJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VimalMavenProjectJenkinsApplication.class, args);
+		 System.out.println("Welcome to COMP367");
+	}
+
+}
---
+++ src/test/java/com/vimal/maven/project/VimalMavenProjectJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vimal.maven.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VimalMavenProjectJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
